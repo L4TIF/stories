deff8fe0c636659eb26079c42717da55d2a18b10
Latiful Rahman <mrlatif598@hotmail.com>
2025-05-25 14:18:51 +0530

    fix stories data location

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Story from './Story'
-import { stories } from '../../public/stories'
+import { stories } from '../../stories'
 
 const StoriesBar = () => {
   return (
